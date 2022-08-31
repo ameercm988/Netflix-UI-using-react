1affbc107f12313094288361e2862421f225afd2
Ameer <ameercm988@gmail.com>
2022-08-31 12:25:01 +0530

    env cofigured

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,5 @@
+require('dotenv').config()
 
 export const baseUrl = 'https://api.themoviedb.org/3';
-export const API_KEY = '6a520c5449b11f6b256b8b79faf48d3a';
+export const API_KEY = process.env.API_KEY;
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
